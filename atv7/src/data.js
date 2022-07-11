33e9b52440edf4797bddad4cfb48541903b4b23e
Maria-Eduarda-080902 <81444876+Maria-Eduarda-080902@users.noreply.github.com>
2022-07-10 21:54:02 -0300

    Add files via upload

---
+++ atv7/src/data.js
@@ -0,0 +1,7 @@
+const students = [
+    {id: 1, name: "Duda", course: "ES", ira: 8.2},
+    {id: 2, name: "Yara", course: "SI", ira: 8.2},
+    {id: 3, name: "Alice", course: "ES", ira: 8.2}
+]
+
+export {students};
